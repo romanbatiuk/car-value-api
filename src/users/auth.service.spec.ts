4883b6bda0cad96e5c806ead3dc9116ca9739c77
romanbatiuk <roman.batiuk87@gmail.com>
2022-04-14 20:47:27 +0300

    updated Unit tests for AuthService, signup & signin

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,25 +1,55 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-it('can create an instance of auth service', async () => {
-	//! Create a fake copy of the users service
-	const fakeUsersService = {
-		find: () => Promise.resolve([]),
-		create: (email: string, password: string) => Promise.resolve({ id: 1, email, password }),
-	};
+describe('AuthService', () => {
+	let service: AuthService;
+	let fakeUsersService: Partial<UsersService>;
 
-	const module = await Test.createTestingModule({
-		providers: [
-			AuthService,
-			{
-				provide: UsersService,
-				useValue: fakeUsersService,
-			},
-		],
-	}).compile();
+	beforeEach(async () => {
+		//* Create a fake copy of the users service
+		fakeUsersService = {
+			find: () => Promise.resolve([]),
+			create: (email: string, password: string) =>
+				Promise.resolve({ id: 1, email, password } as User),
+		};
 
-	const service = module.get(AuthService);
+		const module = await Test.createTestingModule({
+			providers: [
+				AuthService,
+				{
+					provide: UsersService,
+					useValue: fakeUsersService,
+				},
+			],
+		}).compile();
 
-	expect(service).toBeDefined();
+		service = module.get(AuthService);
+	});
+
+	it('can create an instance of auth service', async () => {
+		expect(service).toBeDefined();
+	});
+
+	it('create a new user with a salted and hashed password', async () => {
+		const user = await service.signup('remi@mail.com', '123456');
+		expect(user.password).not.toEqual('123456');
+		const [salt, hash] = user.password.split('.');
+		expect(salt).toBeDefined();
+		expect(hash).toBeDefined();
+	});
+
+	it('throws an error if user signs up with email that is in use', async () => {
+		fakeUsersService.find = () => Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+		try {
+			await service.signup('remi@mail.com', '123456');
+		} catch (err) {}
+	});
+
+	it('throws if singnin is called with an unused email', async () => {
+		try {
+			await service.signup('remi@mail.com', '123456');
+		} catch (err) {}
+	});
 });

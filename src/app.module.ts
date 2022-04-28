f8118247f24734cb5ee835e3064bc09535c6e948
romanbatiuk <roman.batiuk87@gmail.com>
2022-04-28 21:38:27 +0300

    settings env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,33 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { User } from './users/user.entity';
 import { Report } from './reports/report.entity';
 import { CookieSessionModule } from 'nestjs-cookie-session';
 
 @Module({
 	imports: [
+		ConfigModule.forRoot({
+			isGlobal: true,
+			envFilePath: `.env.${process.env.NODE_ENV}`,
+		}),
 		UsersModule,
 		ReportsModule,
 		CookieSessionModule.forRoot({
 			session: { secret: 'keyboard cat' },
 		}),
-		TypeOrmModule.forRoot({
-			type: 'sqlite',
-			database: 'db.sqlite',
-			entities: [User, Report],
-			synchronize: true,
+
+		TypeOrmModule.forRootAsync({
+			inject: [ConfigService],
+			useFactory: (config: ConfigService) => {
+				return {
+					type: 'sqlite',
+					database: config.get<string>('DB_NAME'),
+					synchronize: true,
+					entities: [User, Report],
+				};
+			},
 		}),
 	],
 	controllers: [AppController],

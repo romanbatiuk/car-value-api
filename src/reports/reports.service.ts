bf192792339673ad0d033a319966b319e33479e5
romanbatiuk <roman.batiuk87@gmail.com>
2022-05-23 21:48:10 +0300

    create new report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+	constructor(@InjectRepository(Report) private reportsRepository: Repository<Report>) {}
+	create(reportDto: CreateReportDto) {
+		const report = this.reportsRepository.create(reportDto);
+		return this.reportsRepository.save(report);
+	}
+}
